fcabbfdb6598da704e479518522fa13cb3044185
Vincent Menger <vmenger@protonmail.com>
2022-11-29 09:48:48 +0100

    Format tests

--- test/test_service.py
+++ test/test_service.py
@@ -10,94 +10,100 @@ def client():
         yield client
 
 
-def test_deidentify_none(client):
+class TestDeduce:
+    def test_deidentify_none(self, client):
 
-    example_data = {'text': None, 'id': '8345'}
+        example_data = {"text": None, "id": "8345"}
 
-    response = client.post("/deidentify",
-                           data=json.dumps(example_data),
-                           headers={"Content-Type": "application/json"})
-    data = response.get_json()
+        response = client.post(
+            "/deidentify",
+            data=json.dumps(example_data),
+            headers={"Content-Type": "application/json"},
+        )
+        data = response.get_json()
 
-    assert data['text'] is None
+        assert data["text"] is None
 
+    def test_deidentify(self, client):
 
-def test_deidentify(client):
-    example_data = utils.load_single_example_text()
-    response = client.post("/deidentify",
-                           data=json.dumps(example_data),
-                           headers={"Content-Type": "application/json"})
-    data = response.get_json()
+        example_data = utils.load_single_example_text()
 
-    # Test whether the patient name is removed
-    assert "Jansen" not in data['text']
+        response = client.post(
+            "/deidentify",
+            data=json.dumps(example_data),
+            headers={"Content-Type": "application/json"},
+        )
+        data = response.get_json()
 
-    # Test whether the patient name has been replaced
-    assert "<PATIENT>" in data['text']
+        # Test whether the patient name is removed
+        assert "Jansen" not in data["text"]
+        # Test whether the patient name has been replaced
+        assert "<PATIENT>" in data["text"]
+        # Test whether other functional parts are still included
+        assert "ontslagen van de kliniek" in data["text"]
 
-    # Test whether other functional parts are still included
-    assert "ontslagen van de kliniek" in data['text']
+    def test_deidentify_date_default(self, client):
+        """
+        Test that dates get deidentified when no dates argument is set.
+        """
 
+        input_data = {"text": "20 maart 2021"}
 
-def test_deidentify_date_default(client):
-    """
-    Test that dates get deidentified when no dates argument is set.
-    """
+        response = client.post(
+            "/deidentify",
+            data=json.dumps(input_data),
+            headers={"Content-Type": "application/json"},
+        )
+        output_data = response.get_json()
 
-    input_data = {
-        'text': "20 maart 2021"
-    }
+        assert output_data["text"] == "<DATUM-1>"
 
-    response = client.post("/deidentify", data=json.dumps(input_data), headers={"Content-Type": "application/json"})
-    output_data = response.get_json()
+    def test_deidentify_date_true(self, client):
+        """
+        Test that dates get deidentified when dates argument is set to true.
+        """
 
-    assert output_data['text'] == "<DATUM-1> 2021"
+        input_data = {"text": "20 maart 2021", "dates": True}
 
+        response = client.post(
+            "/deidentify",
+            data=json.dumps(input_data),
+            headers={"Content-Type": "application/json"},
+        )
+        output_data = response.get_json()
 
-def test_deidentify_date_true(client):
-    """
-    Test that dates get deidentified when dates argument is set to true.
-    """
+        assert output_data["text"] == "<DATUM-1>"
 
-    input_data = {
-        'text': "20 maart 2021",
-        'dates': True
-    }
+    def test_deidentify_without_dates(self, client):
+        """
+        Test that dates do not get deidentified when dates argument is set to false.
+        """
 
-    response = client.post("/deidentify", data=json.dumps(input_data), headers={"Content-Type": "application/json"})
-    output_data = response.get_json()
+        input_data = {"text": "20 maart 2021", "dates": False}
 
-    assert output_data['text'] == "<DATUM-1> 2021"
+        response = client.post(
+            "/deidentify",
+            data=json.dumps(input_data),
+            headers={"Content-Type": "application/json"},
+        )
+        output_data = response.get_json()
 
+        assert output_data["text"] == input_data["text"]
 
-def test_deidentify_without_dates(client):
-    """
-    Test that dates do not get deidentified when dates argument is set to false.
-    """
+    def test_deidentify_bulk(self, client):
 
-    input_data = {
-        'text': "20 maart 2021",
-        'dates': False
-    }
+        example_data_bulk = utils.load_multiple_example_texts()
 
-    response = client.post("/deidentify", data=json.dumps(input_data), headers={"Content-Type": "application/json"})
-    output_data = response.get_json()
+        response = client.post(
+            "/deidentify_bulk",
+            data=json.dumps(example_data_bulk),
+            headers={"Content-Type": "application/json"},
+        )
+        data = response.get_json()
 
-    assert output_data['text'] == input_data['text']
-
-
-def test_deidentify_bulk(client):
-    example_data_bulk = utils.load_multiple_example_texts()
-    response = client.post("/deidentify_bulk",
-                           data=json.dumps(example_data_bulk),
-                           headers={"Content-Type": "application/json"})
-    data = response.get_json()
-
-    # Test whether multiple results are returned
-    assert len(data['texts']) == 2
-
-    # Test whether de-identification of the second text was done correctly
-    assert "Jong" not in data['texts'][1]['text']
-
-    # Test whether the adjective use of "jong" was included
-    assert "jong" in data['texts'][1]['text']
+        # Test whether multiple results are returned
+        assert len(data["texts"]) == 2
+        # Test whether de-identification of the second text was done correctly
+        assert "Jong" not in data["texts"][1]["text"]
+        # Test whether the adjective use of "jong" was included
+        assert "jong" in data["texts"][1]["text"]
